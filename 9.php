c1b0c0537c1d2b5c59c355343923c49fdb13049b
Ahmed Abdelhafez <a.emad23545@gmail.com>
2021-11-06 09:07:06 +0200

    Cookies&sessions

--- 9.php
+++ 9.php
@@ -1,6 +1,15 @@
 <?php include "functions.php" ?>
 <?php include "includes/header.php" ?>
+<?php 
 
+session_start();
+
+$_SESSION["message"] = "This is session";
+
+$expire = time() + (60*60*24*7);
+setcookie("One_week_cookie", "Just week", $expire);
+
+?>
 
 
 	<section class="content">
@@ -16,17 +25,26 @@
 			<article class="main-content col-xs-8">
 			
 		
-	
+	<a href="9.php?id='10'">Click Here</a>
 	<?php 
 
-	/*  Create a link saying Click Here, and set 
-	the link href to pass some parameters and use the GET super global to see it
-
-		Step 2 - Set a cookie that expires in one week
-
-		Step 3 - Start a session and set it to value, any value you want.
-	*/
+	//Create a link saying Click Here, and set the link href to pass some parameters and use the GET super global to see it
+	if(isset($_GET["id"])){
+		echo $_GET["id"] . "<br>"; 
+	}
 	
+	//Step 2 - Set a cookie that expires in one week
+
+	if(isset($_COOKIE["One_week_cookie"])){
+		echo $_COOKIE["One_week_cookie"] . "<br>";
+	}
+
+	//Step 3 - Start a session and set it to value, any value you want.
+	
+	if(isset($_SESSION["message"])){
+		echo $_SESSION["message"];
+	}
+
 	?>
 
 
